b837a9bbbe6ce03fa7d288597c8db3c581319bb1
Kevin Cittadini <cittadini.kevin@gmail.com>
2019-04-17 17:20:13 +0200

    2333: Restore _handleAjaxt tests.

--- tests/Soisy/ClientTest.php
+++ tests/Soisy/ClientTest.php
@@ -1,8 +1,9 @@
 <?php
 
 use Soisy\Client;
+use SoisyPlugin\Includes\Settings;
 
-class ClientTest extends WP_UnitTestCase
+class ClientTest extends WP_Ajax_UnitTestCase
 {
 
     /** @var Client */
@@ -105,6 +106,54 @@ class ClientTest extends WP_UnitTestCase
         $this->assertArrayHasKey('min', $response);
         $this->assertArrayHasKey('median', $response);
         $this->assertArrayHasKey('max', $response);
+    }
+
+    /**
+     * @test
+     */
+    public function product_ajax_call()
+    {
+        $this->_setRole('administrator');
+
+        $_POST['_nonce'] = wp_create_nonce('soisy_product_loan_info_block');
+        $_POST['price']  = 655;
+
+        try {
+            $this->_handleAjax('soisy_product_loan_info_block');
+        } catch (WPAjaxDieContinueException $e) {
+
+            $expectedResponse = [
+                'data'   => __('Loan quote text', 'soisy'),
+                'object' => Settings::LOAN_QUOTE_CSS_CLASS
+            ];
+            $actualResponse = json_decode($this->_last_response, true);
+
+            $this->assertEquals($expectedResponse, $actualResponse);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function cart_ajax_call()
+    {
+        $this->_setRole('administrator');
+
+        $_POST['_nonce'] = wp_create_nonce('soisy_cart_loan_info_block');
+        $_POST['price']  = 655;
+
+        try {
+            $this->_handleAjax('soisy_cart_loan_info_block');
+        } catch (WPAjaxDieContinueException $e) {
+
+            $expectedResponse = [
+                'data'   => __('Cart loan quote text', 'soisy'),
+                'object' => Settings::CART_LOAN_QUOTE_CSS_CLASS
+            ];
+            $actualResponse = json_decode($this->_last_response, true);
+
+            $this->assertEquals($expectedResponse, $actualResponse);
+        }
     }
 
     public function errorMessagesDataProvider(): array
