28b796b8519ca8e83b03150f285410f8f7693ed1
salvatore-disavio <salvatore.disavio99@gmail.com>
2022-06-15 11:46:21 +0200

    Add ${VERSION}

--- src/soisy-woocommerce-plugin.php
+++ src/soisy-woocommerce-plugin.php
@@ -3,7 +3,7 @@
 	 * Plugin Name: Soisy Pagamento Rateale
 	 * Plugin URI: https://doc.soisy.it/it/Plugin/WooCommerce.html
 	 * Description: Soisy, la piattaforma di prestiti p2p che offre ai tuoi clienti il pagamento a rate
-	 * Version: 5.2.6
+	 * Version: ${VERSION}
 	 * Author: Soisy
 	 * Author URI: https://www.soisy.it
 	 * Text Domain: soisy
